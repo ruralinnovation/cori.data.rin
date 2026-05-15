0aa8a89a47d3a753a2a34d0488f2cf1b4b69b908
John Hall <john.hall@ruralinnovation.us>
2026-05-15 12:13:42 -0400

    fix caldwell county map location

--- R/archive/create_map_dta.R
+++ R/archive/create_map_dta.R
@@ -8,7 +8,7 @@ i_am("R/create_map_dta.R")
 
 # Commns tracks RIN community updates in the Google sheet [RIN Communties on Website](https://docs.google.com/spreadsheets/d/1fDGclR7GqncaLdkEipOJLLwDn3h4gYHVRcJtP_K1wcU)
 comms_sheet_id <- "1fDGclR7GqncaLdkEipOJLLwDn3h4gYHVRcJtP_K1wcU"
-comms_csv_path <- here("data/[RIN Communties on Website - Current_Communities.csv")
+comms_csv_path <- here("data/RIN Communties on Website - Current_Communities.csv")
 readr::write_csv(googlesheets4::read_sheet(comms_sheet_id, sheet = "Current_Communities"), comms_csv_path)
 
 # This list is maintained in the Google sheet [RIN Map Dataset](https://docs.google.com/spreadsheets/d/1xNuoDexYblr2WGTsTUD5M3lbwWrLNhpmUqp0yXJyGrM)
